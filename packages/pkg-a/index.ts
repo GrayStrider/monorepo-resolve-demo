62539c7e6dd9d9e02bde3930eefa7653d10056ef
Gray Strider <zhoga.ivan@gmail.com>
2020-03-07 19:25:09 +1100

    duplicate shared packages
    still maintains one installation, so only dependency definitions are duplicated

--- packages/pkg-a/index.ts
+++ packages/pkg-a/index.ts
@@ -2,6 +2,7 @@ import B from '@resolve-demo/pkg-b'
 import {pipe, prop} from 'sanctuary'
 
 import {identity} from 'ramda'
+import produce from 'immer'
 
 export default class A {
 	hello = 'world'
@@ -19,3 +20,10 @@ pipe([prop('name'), (s) => `Hello, ${s}`, console.log])(B.name)
  * Try typing 'zip', 'zipWith', produce etc.; you have to manually add it to import
  */
 identity(B.name)
+
+
+/**
+ * After duplicating the dependencies in package.json,
+ * imports work as expected; no new local copies created on yarn install
+ */
+produce(() => {})
